fe3edb8ba485e47464d2f5006d22d0bb5ffa8aca
dhanushkiren <dhanushrameshkvpt03@gmail.com>
2024-11-22 22:57:27 +0530

    modified

--- environment.d.ts
+++ environment.d.ts
@@ -8,13 +8,6 @@ declare global {
       NEXT_PUBLIC_EMAILJS_SERVICE_ID: string;
       NEXT_PUBLIC_EMAILJS_TEMPLATE_ID: string;
       NEXT_PUBLIC_EMAILJS_PUBLIC_KEY: string;
-
-      // google verification key (optional) - used for domain verification on https://search.google.com/search-console
-      NEXT_PUBLIC_GOOGLE_VERIFICATION_KEY: string;
-
-      // google recaptcha keys
-      NEXT_PUBLIC_RECAPTCHA_SITE_KEY: string;
-      RECAPTCHA_SECRET_KEY: string;
     }
   }
 }
